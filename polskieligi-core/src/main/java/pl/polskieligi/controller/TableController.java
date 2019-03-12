fb8f2d54bf94125dbeac468420ef23b64b22265b
User <User@User-Komputer>
2019-03-12 21:19:03 +0100

    thymeleaf

--- polskieligi-core/src/main/java/pl/polskieligi/controller/TableController.java
+++ polskieligi-core/src/main/java/pl/polskieligi/controller/TableController.java
@@ -35,7 +35,7 @@ public class TableController {
 		List<TableRow> rows = tableDAO.getTableRows(projectId);
 		Project p = projectDAO.find(projectId);
 		String title = p.getName();
-		ModelAndView mv = new ModelAndView("table");
+		ModelAndView mv = new ModelAndView("table2");
 		mv.addObject("project_name", title);
 		mv.addObject("rows", rows);
 		mv.addObject("rowsHome", sortRowsHome(rows));
