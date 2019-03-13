80e958bc5c58f221d3311cadf2da5b4fcea0078a
Jałmużna Marcin <marcin.jalmyzna@coi.gov.pl>
2019-03-13 14:42:21 +0100

    sb admin

--- polskieligi-admin/src/main/java/pl/polskieligi/SpringBootAdminServerApplication.java
+++ polskieligi-admin/src/main/java/pl/polskieligi/SpringBootAdminServerApplication.java
@@ -2,17 +2,19 @@ package pl.polskieligi;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
-public class SpringBootAdminServerApplication {
-	public static void main(String[] args) {
+public class SpringBootAdminServerApplication
+		extends SpringBootServletInitializer {
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootAdminServerApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootAdminServerApplication.class, args);
 	}
 }
