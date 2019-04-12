2e81239887fb4f425b69675ec7fb267613aa9bd7
Jałmużna Marcin <marcin.jalmyzna@coi.gov.pl>
2019-04-12 11:53:26 +0200

    MappedSuperclass

--- polskieligi-core/src/main/java/pl/polskieligi/model/Player.java
+++ polskieligi-core/src/main/java/pl/polskieligi/model/Player.java
@@ -3,114 +3,25 @@ package pl.polskieligi.model;
 import pl.polskieligi.log.DisplayUtils;
 
 import javax.persistence.*;
-import java.sql.Date;
 import java.util.List;
 
 @Entity @Table(indexes = {@Index(name = "IDX_PL_MINUT_ID", columnList = "minut_id", unique = false)})
-public class Player {
+public class Player extends  Person {
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private Integer minut_id;
-
-	private String name;
-	private String surname;
 	private String pseudo;
-	private Date birthDate;
-	private String birthTown;
-	private String birthCountry;
-	private Date deathDate;
-	private String deatTown;
-	private String deatCountry;
-	private String nationality;
 	private String position;
 	private Float weight;
 	private Float height;
-
-	private Integer importStatus;
 	
 	@OneToMany
 	@JoinColumn(name = "player_id")
 	private List<TeamLeaguePlayer> teamleaguePlayers; 
 
 	public Player() {
-		minut_id = 0;
-		name = "";
-		surname = "";
-		birthDate = null;
-		birthTown = "";
-		birthCountry = "";
-		deathDate = null;
-		deatTown = "";
-		deatCountry = "";
-		nationality = "";
+		pseudo = "";
 		position = "";
 		weight = new Float(0);
 		height = new Float(0);
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getMinut_id() {
-		return minut_id;
-	}
-
-	public void setMinut_id(Integer minut_id) {
-		this.minut_id = minut_id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getBirthTown() {
-		return birthTown;
-	}
-
-	public void setBirthTown(String birthTown) {
-		this.birthTown = birthTown;
-	}
-
-	public String getBirthCountry() {
-		return birthCountry;
-	}
-
-	public void setBirthCountry(String birthCountry) {
-		this.birthCountry = birthCountry;
-	}
-
-	public String getNationality() {
-		return nationality;
-	}
-
-	public void setNationality(String nationality) {
-		this.nationality = nationality;
 	}
 
 	public String getPosition() {
@@ -137,46 +48,14 @@ public class Player {
 		this.height = height;
 	}
 
-	public Date getDeathDate() {
-		return deathDate;
-	}
-
-	public void setDeathDate(Date deathDate) {
-		this.deathDate = deathDate;
-	}
-
-	public String getDeatTown() {
-		return deatTown;
-	}
-
-	public void setDeatTown(String deatTown) {
-		this.deatTown = deatTown;
-	}
-
-	public String getDeatCountry() {
-		return deatCountry;
-	}
-
-	public void setDeatCountry(String deatCountry) {
-		this.deatCountry = deatCountry;
-	}
-
-	public Integer getImportStatus() {
-		return importStatus;
-	}
-
-	public void setImportStatus(Integer importStatus) {
-		this.importStatus = importStatus;
-	}
-
 	public String getFullName() {
-		return DisplayUtils.getPseudo(name)+' '+DisplayUtils.getPseudo(surname);
+		return DisplayUtils.getPseudo(getName())+' '+DisplayUtils.getPseudo(getSurname());
 	}
 
 
 	@Override public String toString() {
-		return id + " " + minut_id + " " +name + " " +surname + " " +birthDate + " " +birthTown + " " +birthCountry+ " "
-				+ deathDate+ " " + deatTown+ " " + deatCountry+ " " + nationality + " " +position + " " + weight + " " + height;
+		return getId() + " " + getMinut_id() + " " +getName() + " " +getSurname() + " " +getBirthDate() + " " +getBirthTown() + " " +getBirthCountry()+ " "
+				+ getDeathDate()+ " " + getDeatTown()+ " " + getDeatCountry()+ " " + getNationality() + " " +position + " " + weight + " " + height;
 	}
 
 	public String getPseudo() {
--- polskieligi-core/src/main/java/pl/polskieligi/model/Referee.java
+++ polskieligi-core/src/main/java/pl/polskieligi/model/Referee.java
@@ -1,121 +1,13 @@
 package pl.polskieligi.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
-import java.sql.Date;
-
 @Entity  @Table(indexes = {@Index(name = "IDX_RE_MINUT_ID", columnList = "minut_id", unique = false)})
-public class Referee {
+public class Referee extends Person{
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private Integer minut_id;
-
-	private String name;
-	private String surname;
-	private Date birthDate;
-	private String birthTown;
-	private String birthCountry;
-	private Date deathDate;
-	private String deatTown;
-	private String deatCountry;
-	private String nationality;
 	private String district;
-
-	private Integer importStatus;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getMinut_id() {
-		return minut_id;
-	}
-
-	public void setMinut_id(Integer minut_id) {
-		this.minut_id = minut_id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getBirthTown() {
-		return birthTown;
-	}
-
-	public void setBirthTown(String birthTown) {
-		this.birthTown = birthTown;
-	}
-
-	public String getBirthCountry() {
-		return birthCountry;
-	}
-
-	public void setBirthCountry(String birthCountry) {
-		this.birthCountry = birthCountry;
-	}
-
-	public Date getDeathDate() {
-		return deathDate;
-	}
-
-	public void setDeathDate(Date deathDate) {
-		this.deathDate = deathDate;
-	}
-
-	public String getDeatTown() {
-		return deatTown;
-	}
-
-	public void setDeatTown(String deatTown) {
-		this.deatTown = deatTown;
-	}
-
-	public String getDeatCountry() {
-		return deatCountry;
-	}
-
-	public void setDeatCountry(String deatCountry) {
-		this.deatCountry = deatCountry;
-	}
-
-	public String getNationality() {
-		return nationality;
-	}
-
-	public void setNationality(String nationality) {
-		this.nationality = nationality;
-	}
 
 	public String getDistrict() {
 		return district;
@@ -125,11 +17,4 @@ public class Referee {
 		this.district = district;
 	}
 
-	public Integer getImportStatus() {
-		return importStatus;
-	}
-
-	public void setImportStatus(Integer importStatus) {
-		this.importStatus = importStatus;
-	}
 }
